46ca7332ea696c517f60bd794a4d31764b44c9a5
O.V. Rud <helvrud@gmail.com>
2018-06-13 11:06:51 +0200

    urls

--- issue/issuetracker/urls.py
+++ issue/issuetracker/urls.py
@@ -9,16 +9,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.views import login, logout
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.static import serve
-
-
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function, unicode_literals
-
-from cms.sitemaps import CMSSitemap
 from django.contrib import admin
-from django.contrib.sitemaps.views import sitemap
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.static import serve
 
 admin.autodiscover()
 
